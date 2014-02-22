8785c69a3d9932b8343c5d7bdccd3454f7618e21
Jason Stone <rolaveric@gmail.com>
2014-02-22 23:36:07 +1100

    Authentication demonstration
    Created code which demonstrates how to do Authentication through
    middleware, and then handle Authorization by registering multiple
    handlers to the same route - One to check Authorization, and one to do
    the work should the check succeed.

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+A generic HTTP server program for demonstrative purposes.
+Uses Martini as the web framework, because it's lightweight.
+*/
+package main
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/codegangsta/martini"
+)
+
+// Type which, through reflection, Martini uses to dependency inject the user
+// found by the Authentication middleware
+type User interface{}
+
+func main() {
+	// Starting a standard martini server,
+	// with logging and panic recovery middleware built in
+	m := martini.Classic()
+
+	// Register middleware which authenticates the client
+	m.Use(Authentication)
+
+	// Register a route which does something with the authenticated user,
+	// but doesn't care for security reasons (ie. No authorization check)
+	m.Get("/", func(u User) string {
+		return fmt.Sprintf("Hello %s!", u)
+	})
+
+	// Register a route which requires the authenticated user to have specific authorization
+	m.Get("/secret", Authorization("secret access"), func(u User) string {
+		return fmt.Sprintf("Hello Secret Agent %s!", u)
+	})
+
+	// Start the server
+	m.Run()
+}
+
+func Authentication(c martini.Context, req *http.Request, res http.ResponseWriter) {
+	// Get the Authorization header
+	a := req.Header.Get("Authorization")
+	if a == "" {
+		// No header, user must be guest
+		c.MapTo("guest", (*User)(nil))
+		return
+	}
+
+	// Remove the 'Basic ' prefix
+	a = strings.TrimPrefix(a, "Basic ")
+
+	// Decode the Authorization header
+	data, err := base64.StdEncoding.DecodeString(a)
+	if err != nil {
+		// Bad Authorization value - return 401 Unauthorized
+		http.Error(res, "Could not decode Authorization header", http.StatusUnauthorized)
+		// Note: If the response gets written to, Martini doesn't call any subsequent handlers
+		return
+	}
+
+	// Split out the username and password
+	s := strings.Split(string(data), ":")
+	if len(s) < 2 {
+		// Bad Authorization value - return 401 Unauthorized
+		http.Error(res, "Authorization header requires a username and password", http.StatusUnauthorized)
+		// Note: If the response gets written to, Martini doesn't call any subsequent handlers
+		return
+	}
+
+	// At this point, you would normally lookup a user database
+	// Since this is an example - we're just accepting the username as is
+	c.MapTo(s[0], (*User)(nil))
+}
+
+func Authorization(access string) func(User, http.ResponseWriter) {
+	// Returning a handler function that checks for the required access
+	return func(u User, res http.ResponseWriter) {
+		// For brevity, we're just checking that the username is 'admin'
+		if u != "admin" {
+			// If not, return a response that stops Martini calling the next handler
+			http.Error(res, "Not Authorized", http.StatusUnauthorized)
+		}
+	}
+}
